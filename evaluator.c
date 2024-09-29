d94e8766bb476c7dc06c8a37c7731d0940fee39d
Kirikaze Chiyuki <me@chyk.ink>
2024-09-29 17:18:32 +0800

    [feat] binary tree evaluator

---
+++ evaluator.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "evaluator.h"
+
+int evaluate(TreeNode* root) {
+    if (root == NULL) {
+        return 0;
+    }
+    if (root->type == NUMBER) {
+        return root->token.number;
+    }
+    int left = evaluate(root->left);
+    int right = evaluate(root->right);
+    switch (root->token.op) {
+        case '+':
+            return left + right;
+        case '-':
+            return left - right;
+        case '*':
+            return left * right;
+        case '/':
+            return left / right;
+        default:
+            return 0;
+    }
+}
---
+++ evaluator.h
@@ -0,0 +1,3 @@
+#include "types.h"
+
+int evaluate(TreeNode* root);
